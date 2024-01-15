349a948e84adbaa27005ff8af1725afbea5ce942
cristian fysight <cristian@fysight.ai>
2024-01-15 09:51:38 -0300

    Add socket rooms

--- sockets.js
+++ sockets.js
@@ -3,27 +3,32 @@ function listen(socketServer) {
   const pongNamespace = socketServer.of("/pong");
   pongNamespace.on("connection", (socket) => {
     console.log("User connected on server as: ", socket.id);
-    socket.on("disconnect", (reason) => {
-      console.log(`User disconnected: ${socket.id} with reason: ${reason}`);
-    });
+    const roomNumber = Math.floor(readyPlayerCount / 2);
+    let roomName = `room ${roomNumber}`;
 
     socket.on("ready", () => {
-      console.log("Player ready", socket.id);
+      socket.join(roomName);
+      console.log(`The player ${socket.id} joined to room ${roomName}`);
 
       readyPlayerCount++;
 
       if (readyPlayerCount % 2 === 0) {
         console.log("Game starting");
-        pongNamespace.emit("start", socket.id);
+        pongNamespace.in(roomName).emit("start", socket.id);
       }
     });
 
     socket.on("paddleMove", (data) => {
-      socket.broadcast.emit("paddleMove", data);
+      socket.to(roomName).emit("paddleMove", data);
     });
 
     socket.on("ballMove", (data) => {
-      socket.broadcast.emit("ballMove", data);
+      socket.to(roomName).emit("ballMove", data);
+    });
+
+    socket.on("disconnect", (reason) => {
+      socket.leave(roomName);
+      console.log(`User disconnected: ${socket.id} with reason: ${reason}`);
     });
   });
 }
